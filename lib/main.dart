a7ea3e15ddc0570b4e366026fe73cb13ecfca474
Mustafe Hassan <mustafehassan01@outlook.com>
2022-06-29 17:15:32 +0300

    removed devicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
 
 import 'constants/constants.dart';
 import 'sign_up.dart';
 
 void main() {
-  runApp(DevicePreview(
-      enabled: true, builder: (context) => const SignUpValidation()));
-}
-
-class MyCustomScrollBehavior extends MaterialScrollBehavior {
-  //* Override behavior methods and getters like dragDevices
-  @override
-  Set<PointerDeviceKind> get dragDevices => {
-        PointerDeviceKind.touch,
-        PointerDeviceKind.mouse,
-      };
+  runApp(const SignUpValidation());
 }
 
 class SignUpValidation extends StatelessWidget {
@@ -26,10 +13,6 @@ class SignUpValidation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      scrollBehavior: MyCustomScrollBehavior(),
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       title: 'SignUp Validation Form',
       theme: theme,
       debugShowCheckedModeBanner: false,
